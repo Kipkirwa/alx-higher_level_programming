ac72920bc404b0da2a3315908556c18529c0992a
Kipkirwa <gleitich@gmail.com>
2023-04-13 04:53:00 -0700

    Python - Input/Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""
-5-save_to_json module
-"""
+"""Defines a JSON file-writing function."""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """
-    save_to_json_file -  writes an Object to a text file,
-                        using a JSON representation:
-    Args:
-        my_obj:
-        filename:
-    Return:
-    """
+    """Write an object to a text file using JSON representation."""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-6-load_from_json_file module
-"""
+"""Defines a JSON file-reading function."""
 import json
 
 
 def load_from_json_file(filename):
-    """
-    load_from_json_file - loads an object from JSON file.
-    Args:
-        filename: name of the file
-    """
-    with open(filename, "r") as f:
-        my_obj = json.load(f)
-        return my_obj
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
