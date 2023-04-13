d96c61f663385963e3a300cc093a0b77b1e9ade5
Kipkirwa <gleitich@gmail.com>
2023-04-13 04:37:36 -0700

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-"""
-7-add_item module
-"""
-import sys
+"""Defines a JSON file-writing function."""
 import json
-import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
 
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
-
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-
-save_to_json_file(json_list, file)
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
